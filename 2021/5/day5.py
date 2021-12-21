03ea0512d54d87ac8b4878ea66dc20fc1367ad63
Perttu Rintanen <perttu.rintanen@ouraring.com>
2021-12-21 17:22:16 +0200

    5

--- 2021/5/day5.py
+++ 2021/5/day5.py
@@ -12,12 +12,17 @@ with open(file_i, 'r') as f:
         coords = [int(n) for n in line.replace('-> ', '').replace(',', ' ').split()]
         x = [coords[0], coords[2]]
         y = [coords[1], coords[3]]
-        x = list(range(min(x), max(x)))
-        y = list(range(min(y), max(y)))
-        if len(x) == 0:
-            x = coords[0]
-        elif len(y) == 0:
-            y = coords[1]
-        big_map[x, y] += 1
+        x = list(range(min(x), max(x) + 1)) if x[0] != x[1] else x[0]
+        y = list(range(min(y), max(y) + 1)) if y[0] != y[1] else y[0]
+        # part 1
+        if coords[0] == coords[2] or coords[1] == coords[3]:
+            big_map[y, x] += 1
+        # part 2 - comment the else clause for part 1 result
+        else:
+            if coords[0] > coords[2]:
+                x.reverse()
+            if coords[1] > coords[3]:
+                y.reverse()
+            big_map[y, x] += 1
 danger_zone = big_map[np.where(big_map >= 2)]
-print(f'{len(danger_zone)} dangerous points')
+print(f'{danger_zone.size} dangerous points')
