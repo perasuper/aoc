bc6e140b15be7cbe2e1822c41604c0e3ed0e9413
Perttu Rintanen <perttu.rintanen@ouraring.com>
2021-12-21 12:50:58 +0200

    d4

--- 2021/4/day4.py
+++ 2021/4/day4.py
@@ -23,7 +23,7 @@ class Bingo:
             items = []
             for j in range(i, i+5):
                 items.append([int(number) for number in self.input_stream[j].split()])
-                new_card = np.array(items)
+            new_card = np.array(items)
             # TODO: jotain järkeä tähän paskaan
             if i == 2:  # first card
                 all_cards = new_card
@@ -33,27 +33,29 @@ class Bingo:
                 all_cards = np.reshape(np.append(all_cards, new_card), (len(all_cards) + 1, 5, 5))
         return all_cards
 
-    def check_cards(self):
-        pass
-
 
 if __name__ == '__main__':
     file_i = path.join(Path(__file__).parent.absolute(), 'input.txt')
     bingo = Bingo(file_i)
-
-    # part 1
-    numbers = bingo.drawn_numbers
-    cards = bingo.game_cards
     checking_cards = np.zeros_like(bingo.game_cards, dtype=int)
-    end = False
+    won_cards = []
     for idx, drawn_number in enumerate(bingo.drawn_numbers):
         checking_cards[np.where(bingo.game_cards == drawn_number)] = 1
+        # Start checking cards after 5 numbers are drawn
         if idx >= 5:
+            # use x as index to check all columns and rows
             for x in range(0, 5):
                 for id_card, checking_card in enumerate(checking_cards):
-                    if all(checking_card[x, :] == 1) or all(checking_card[:, x] == 1):
-                        winning_card = cards[id_card, :, :]
+                    if (all(checking_card[x, :] == 1) or all(checking_card[:, x] == 1)) and id_card not in won_cards:
+                        winning_card = bingo.game_cards[id_card, :, :]
                         score = sum(winning_card[np.where(checking_card == 0)]) * drawn_number
-                        print(f'BINGO\n{winning_card}\n{checking_card}')
-                        print(f'score: {score}')
-
+                        # part 1
+                        if len(won_cards) == 0:
+                            print(f'BINGO ({idx}th number)\n{winning_card}\n{checking_card}')
+                            print(f'score: {score}, winning card: {id_card}')
+                        # part 2
+                        elif len(won_cards) == len(bingo.game_cards) - 1:
+                            print(f'\nlast card to get bingo ({idx}th number)\n{winning_card}\n{checking_card}')
+                            print(f'score: {score}, winning card: {id_card}')
+                            db = 1
+                        won_cards.append(id_card)
