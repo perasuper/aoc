fc2bcabb822b3fbf2eb0d14802ed7f8ce123ee7b
perasuper <61082376+perasuper@users.noreply.github.com>
2021-12-22 18:36:41 +0200

    d8

--- 2021/8/day8.py
+++ 2021/8/day8.py
@@ -8,25 +8,23 @@ def diff(a, b):
     return "".join([i for i in a if i not in b])
 
 
-counts = [0] * 4
+pt1_counts = [0] * 4
 
-file_i = path.join(Path(__file__).parent.absolute(), 'example.txt')
+file_i = path.join(Path(__file__).parent.absolute(), 'input.txt')
 with open(file_i) as f:
+    sum_all = 0
     for line in f:
-# line = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
         numbers = [''] * 10
         signal = line.split('|')[0].split()
         output = line.split('|')[1].split()
-        signal_digit_lengths = np.char.str_len(signal)
-        # counts = [counts[0] + (signal_digit_lengths == 2).sum(), counts[1] + (signal_digit_lengths == 4).sum(), counts[2] +
-        #           (signal_digit_lengths == 3).sum(), counts[3] + (signal_digit_lengths == 7).sum()]
-
+        output_digit_lengths = np.char.str_len(output)
+        pt1_counts = [pt1_counts[0] + (output_digit_lengths == 2).sum(), pt1_counts[1] + (output_digit_lengths == 4).sum(), pt1_counts[2] +
+                      (output_digit_lengths == 3).sum(), pt1_counts[3] + (output_digit_lengths == 7).sum()]
 
         numbers[7] = list(filter(lambda x: len(x) == 3, signal))[0]
         numbers[1] = list(filter(lambda x: len(x) == 2, signal))[0]
         numbers[4] = list(filter(lambda x: len(x) == 4, signal))[0]
         numbers[8] = list(filter(lambda x: len(x) == 7, signal))[0]
-
 
         five_longs = np.unique(list(filter(lambda x: len(x) == 5, signal)))
         six_longs = np.unique(list(filter(lambda x: len(x) == 6, signal)))
@@ -54,8 +52,14 @@ with open(file_i) as f:
         output_decoded = ''
         for o in output:
             for idx, num in enumerate(numbers):
-                if all(l in num for l in o):
-                    output_decoded += str(idx)
-                    break
-        print(output_decoded)
+                if len(o) == len(num):
+                    if idx == 8:
+                        db = 1
+                    if all(l in num for l in o):
+                        output_decoded += str(idx)
+                        break
+        sum_all += int(output_decoded)
+        print(f'decoded output: {output_decoded}')
 
+print(f'part 1 {pt1_counts}, {sum(pt1_counts)}')
+print(f'part 2 sum of all outputs: {sum_all}')
